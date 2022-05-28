da5e4fb472de0d3479f0a0243a91f26aac409c05
equipoLmj2 <equipolmj2@gmail.com>
2022-05-27 23:57:22 -0500

    POO en proceso...

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,8 @@ public class Main {
             System.out.println(mangoTommy.getName() + " " + mangoTommy.getAverageWeight() + "Kg "  + mang.getName());
         }
 
+        // en proceso
+
 
 
 
